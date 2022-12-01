6f7eb46c571cb83beb7687ff419360bccce17cc1
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-01 20:58:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int Gimme(double[] inputArray)
+    {
+        return Array.IndexOf(inputArray, inputArray.OrderBy(x => x).ToArray()[1]);
+    }
+}
